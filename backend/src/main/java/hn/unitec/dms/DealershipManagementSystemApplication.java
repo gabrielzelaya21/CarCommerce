ac709c4c2f4a53f6e9fdd355695b66a14b5e4da4
Gabriel Zelaya <gabriel.zelayaa21@gmail.com>
2025-02-16 22:52:39 -0600

    Boilerplate code

---
+++ backend/src/main/java/hn/unitec/dms/DealershipManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package hn.unitec.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealershipManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealershipManagementSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/hn/unitec/dms/DealershipManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unitec.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealershipManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
